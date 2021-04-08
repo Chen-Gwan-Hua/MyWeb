8c5c485dbaef57cf2025a759b94d870464e4182a
ltyepc <ltyepc5698@gmail.com>
2021-04-08 17:46:22 +0800

    CSS新增、JS滾動動畫、MapApi

--- CSS/Scroll.js
+++ CSS/Scroll.js
@@ -1,23 +1,57 @@
-var alink = document.getElementsByClassName("anchor")
+/*var alink = document.getElementsByClassName("anchor")
 
-for(var i = 0; i < alink.length; i++){
-	alink[i].addEventListener('click', function(){
-		let anchor = this.getAttribute('href');
-		let topPos = document.getElementById(anchor).offsetTop;
-		window.scroll(0,topPos);
-		return false;
-	})
-}
+for (var i = 0; i < alink.length; i++) {
+    alink[i].addEventListener('click', function() {
+        let anchor = this.getAttribute('href');
+        console.log(anchor);
+        let topPos = document.getElementById(anchor).offsetTop;
+        window.scroll(0, topPos);
+        return false;
+    })
+}*/
 
 const toTop = document.querySelector(".to-top");
 window.addEventListener("scroll", () => {
-	if(window.pageYOffset > 10){
-		toTop.classList.add("active");
-	}else{
-		toTop.classList.remove("active");
-	}
+    if (window.pageYOffset > 10) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
 })
 
-	
-
-
+window.addEventListener('scroll', function() {
+        let ySet = document.querySelector('#anchor1').offsetTop;
+        const yLet = document.querySelector('.introduce')
+        if (window.pageYOffset >= ySet - 100) {
+            yLet.classList.add('active');
+        } else {
+            yLet.classList.remove('active');
+        }
+    })
+    /*window.addEventListener('scroll', function() {
+        let ySet = document.querySelector('#anchor1').offsetTop;
+        const yLet = document.querySelector('.introduce')
+        if (window.pageYOffset >= ySet - 100) {
+            yLet.classList.add('active');
+        } else {
+            yLet.classList.remove('active');
+        }
+    })
+    window.addEventListener('scroll', function() {
+        let ySet = document.querySelector('#anchor1').offsetTop;
+        const yLet = document.querySelector('.introduce')
+        if (window.pageYOffset >= ySet - 100) {
+            yLet.classList.add('active');
+        } else {
+            yLet.classList.remove('active');
+        }
+    })
+    window.addEventListener('scroll', function() {
+        let ySet = document.querySelector('#anchor1').offsetTop;
+        const yLet = document.querySelector('.introduce')
+        if (window.pageYOffset >= ySet - 100) {
+            yLet.classList.add('active');
+        } else {
+            yLet.classList.remove('active');
+        }
+    })*/
