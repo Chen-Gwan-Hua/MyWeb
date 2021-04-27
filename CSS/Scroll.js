fb3d6408662ddfbbc68c26afdbb4860c2b8941da
ltyepc <ltyepc5698@gmail.com>
2021-04-27 14:29:29 +0800

    Rfile

--- CSS/Scroll.js
+++ CSS/Scroll.js
@@ -55,3 +55,67 @@ window.addEventListener('scroll', function() {
             yLet.classList.remove('active');
         }
     })*/
+
+// import axios from 'axios';
+
+// axios.get('http://localhost:3000/users')
+//     .then(function(response) {
+//         console.log('11');
+//         console.log(response.data); // api 回傳的資料會在這裡
+//         console.log(response.status); // 這個是 HTTP狀態碼
+//         console.log(response.config); // 這個 Request 的 config
+//     });
+// axios({
+//         method: 'get',
+//         url: 'http://localhost:3000/users',
+//         params: {
+//             id: 3
+//         }
+//     })
+//     .then(function(response) {
+//         console.log(response.data);
+//         console.log(response.status); // 這個是 HTTP狀態碼
+//         console.log(response.config);
+//         alert('ewwe');
+//     })
+//     .catch(function(error) {
+//         console.log('fall');
+//     })
+/* axios.post('http://localhost:3000/users', { // 要傳送的資料由後方物件帶入
+        firstName: 'Fred',
+        lastName: 'Flintstone'
+    })
+    .then(function(response) {
+        console.log(response);
+    })
+    .catch(function(error) {
+        console.log(error);
+    }); */
+
+/*window.addEventListener('scroll', function() {
+    let ySet = document.querySelector('#anchor1').offsetTop;
+    const yLet = document.querySelector('.introduce')
+    if (window.pageYOffset >= ySet - 100) {
+        yLet.classList.add('active');
+    } else {
+        yLet.classList.remove('active');
+    }
+})
+window.addEventListener('scroll', function() {
+    let ySet = document.querySelector('#anchor1').offsetTop;
+    const yLet = document.querySelector('.introduce')
+    if (window.pageYOffset >= ySet - 100) {
+        yLet.classList.add('active');
+    } else {
+        yLet.classList.remove('active');
+    }
+})
+window.addEventListener('scroll', function() {
+    let ySet = document.querySelector('#anchor1').offsetTop;
+    const yLet = document.querySelector('.introduce')
+    if (window.pageYOffset >= ySet - 100) {
+        yLet.classList.add('active');
+    } else {
+        yLet.classList.remove('active');
+    }
+})*/
